958da7a0e008788d2390b6dac496b822299d6e92
darkradish <marceau.guillaume@gmail.com>
2025-02-03 19:39:53 +0100

    fix: usage of official function for chroma

--- application/src/infrastructure/vectorial/database.py
+++ application/src/infrastructure/vectorial/database.py
@@ -4,11 +4,9 @@ import uuid
 from typing import List, Union
 
 from chromadb.utils.embedding_functions import create_langchain_embedding
-from chromadb.api.types import Documents, EmbeddingFunction, Embeddings, Images
 
 from langchain_chroma import Chroma
 
-from langchain_core.embeddings import Embeddings as LangchainEmbeddings
 from langchain_core.vectorstores import VectorStoreRetriever
 
 from src.application.config import AppConfig
@@ -40,7 +38,9 @@ class VectorialDataBase:
                 self.coll_name = f"{coll_prefix}_{config.dbvec.collection}"
 
             # self.embeddings = HuggingFaceEmbeddings(model_name=config.dbvec.model)
-            self.embeddings = Llm(config, embeddings=True).embeddings
+            self.embeddings = create_langchain_embedding(
+                Llm(config, embeddings=True).embeddings
+            )
 
         else:
             raise ValueError(f"{str(config.dbvec.client)} not yet supported")
@@ -79,16 +79,18 @@ class VectorialDataBase:
                 except Exception:
                     pass
             self.collection = self.client.get_or_create_collection(
-                self.coll_name, embedding_function=self.ChromaLangchainEmbeddingFunction(self.embeddings)
+                self.coll_name,
+                embedding_function=self.embeddings,
             )
             # Create a Chroma clientdb using the initialized client, collection and embeddings.
             self.clientdb = Chroma(
                 client=self.client,
                 collection_name=self.coll_name,
-                embedding_function=self.ChromaLangchainEmbeddingFunction(self.embeddings),
+                embedding_function=self.embeddings,
             )
             self.client.get_collection(
-                self.coll_name, embedding_function=self.ChromaLangchainEmbeddingFunction(self.embeddings)
+                self.coll_name,
+                embedding_function=self.embeddings,
             )
 
     def get_retriever(self, words_number=2) -> VectorStoreRetriever:
@@ -101,7 +103,9 @@ class VectorialDataBase:
         :return: A VectorStoreRetriever instance
         """
         self.logger.debug("Return receiver with words_number=%s", str(words_number))
-        return self.clientdb.as_retriever(search_kwargs={"k": words_number, "fetch_k": 5}, search_type="mmr")
+        return self.clientdb.as_retriever(
+            search_kwargs={"k": words_number, "fetch_k": 5}, search_type="mmr"
+        )
 
     # Add a document to the collection.
 
@@ -144,55 +148,3 @@ class VectorialDataBase:
         for doc in chunked_documents:
             # Add each document to the collection.
             self.add_to_collection(doc)
-
-    class ChromaLangchainEmbeddingFunction(
-        LangchainEmbeddings, EmbeddingFunction[Union[Documents, Images]]  # type: ignore
-    ):
-        """
-        This class is used as bridge between langchain embedding functions and custom chroma embedding functions.
-        """
-
-        def __init__(self, embedding_function: LangchainEmbeddings) -> None:
-            """
-            Initialize the ChromaLangchainEmbeddingFunction
-
-            Args:
-                embedding_function : The embedding function implementing Embeddings from langchain_core.
-            """
-            self.embedding_function = embedding_function
-
-        def embed_documents(self, documents: Documents) -> List[List[float]]:
-            return self.embedding_function.embed_documents(documents)  # type: ignore
-
-        def embed_query(self, query: str) -> List[float]:
-            return self.embedding_function.embed_query(query)  # type: ignore
-
-        def embed_image(self, uris: List[str]) -> List[List[float]]:
-            if hasattr(self.embedding_function, "embed_image"):
-                return self.embedding_function.embed_image(uris)  # type: ignore
-            else:
-                raise ValueError(
-                    "The provided embedding function does not support image embeddings."
-                )
-
-        def __call__(self, input: Documents) -> Embeddings:  # type: ignore
-            """
-            Get the embeddings for a list of texts or images.
-
-            Args:
-                input (Documents | Images): A list of texts or images to get embeddings for.
-                Images should be provided as a list of URIs passed through the langchain data loader
-
-            Returns:
-                Embeddings: The embeddings for the texts or images.
-
-            Example:
-                >>> langchain_embedding = ChromaLangchainEmbeddingFunction(embedding_function=OpenAIEmbeddings(model="text-embedding-3-large"))
-                >>> texts = ["Hello, world!", "How are you?"]
-                >>> embeddings = langchain_embedding(texts)
-            """
-            # Due to langchain quirks, the dataloader returns a tuple if the input is uris of images
-            if input[0] == "images":
-                return self.embed_image(list(input[1]))  # type: ignore
-
-            return self.embed_documents(list(input))  # type: ignore
